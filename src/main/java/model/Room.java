5a19a1bea2718ef159943b13191e212a3ddb70f9
hominhduc18 <hoduc092@gmail.com>
2023-12-23 07:10:06 +0700

    sett_up

---
+++ src/main/java/model/Room.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Room {
+	
+	private Long id;
+	private String roomType;
+	private BigDecimal roomPrice;
+	private boolean isBooked = false;
+	private List<BookRoom> bookings;
+	
+	public Room() {
+
+
+		this.bookings = new ArrayList<>();
+	}
+	
+	
+
+}
